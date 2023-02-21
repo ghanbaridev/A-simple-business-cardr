8b8e32132dd3eeee3f5e0e098314f56bb4e75451
ghanbaridev <104554904+ghanbaridev@users.noreply.github.com>
2023-02-21 17:19:24 +0330

    added font

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 4, 149, 120),
+      backgroundColor: Color.fromARGB(255, 5, 122, 99),
       body: SafeArea(
         child: Column(children: [
           CircleAvatar(
             radius: 50,
             backgroundImage: AssetImage('images/m.jpg'),
           ),
+          Text(
+            "Mohammd Ghanbari",
+            style: TextStyle(
+                fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold),
+          )
         ]),
       ),
     ));
